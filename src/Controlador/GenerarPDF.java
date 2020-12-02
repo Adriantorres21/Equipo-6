2a258b3a8cb736647e0eb245c8fe9c1e548c908b
Adriantorres21 <64857184+Adriantorres21@users.noreply.github.com>
2020-12-02 00:53:34 -0600

    Clase para generar PDF

---
+++ src/Controlador/GenerarPDF.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Adrian
+ */
+public class GenerarPDF {
+    String ruta = "C:\\Users\\Adrian\\Desktop\\miPDF.pdf";
+    public static void main(String[] args) {
+        
+    }
+    
+    public  void crearPDF(String ruta) throws IOException{
+       
+        
+    }
+}
